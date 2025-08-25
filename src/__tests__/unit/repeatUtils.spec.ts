88d853d29de284db69866641cdaeb9d1c9299ae8
김수현 <kimsuhyeon@gimsuhyeon-ui-MacBookPro.local>
2025-08-26 01:16:17 +0900

    fix : GREEN --> 매일 반복 이벤트 폼 사용해서 구현

--- src/__tests__/unit/repeatUtils.spec.ts
+++ src/__tests__/unit/repeatUtils.spec.ts
@@ -1,12 +1,23 @@
 import { repeatingDates } from '../../utils/repeatingDates';
+import { Event } from '../../types';
 
 describe('repeatingDates', () => {
   it('매일 반복 --> 시작일부터 종료일까지 매일 생성한다', () => {
-    const result = repeatingDates('2025-10-01', {
-      type: 'daily',
-      interval: 1,
-      endDate: '2025-10-05',
-    });
+    const events: Event[] = [
+      {
+        id: '1',
+        title: '이벤트 1',
+        date: '2025-10-01',
+        startTime: '10:00',
+        endTime: '11:00',
+        description: '이벤트 1 설명',
+        location: '이벤트 1 장소',
+        category: '이벤트 1 카테고리',
+        repeat: { type: 'daily', interval: 1, endDate: '2025-10-05' },
+        notificationTime: 10,
+      },
+    ];
+    const result = repeatingDates(events[0]);
     expect(result).toEqual(['2025-10-01', '2025-10-02', '2025-10-03', '2025-10-04', '2025-10-05']);
   });
 
