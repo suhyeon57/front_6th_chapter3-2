81f9ee3c177bac65be7fe89d4c6ec42a26032da2
김수현 <kimsuhyeon@gimsuhyeon-ui-MacBookPro.local>
2025-08-25 23:36:53 +0900

    feat : RED --> 매일 반복 테스트 코드 작성

--- src/__tests__/unit/repeatUtils.spec.ts
+++ src/__tests__/unit/repeatUtils.spec.ts
@@ -1,7 +1,14 @@
 import { repeatingDates } from '../../utils/repeatingDates';
 
 describe('repeatingDates', () => {
-  it('매일 반복 --> 시작일부터 종료일까지 매일 생성한다', () => {});
+  it('매일 반복 --> 시작일부터 종료일까지 매일 생성한다', () => {
+    const result = repeatingDates('2025-10-01', {
+      type: 'daily',
+      interval: 1,
+      endDate: '2025-10-05',
+    });
+    expect(result).toEqual(['2025-10-01', '2025-10-02', '2025-10-03', '2025-10-04', '2025-10-05']);
+  });
 
   it('매주 반복 --> 시작일부터 종료일까지 매주 생성한다', () => {});
 
