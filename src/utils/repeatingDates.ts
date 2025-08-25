49ce304b3676f9d2241c5253d25733f43a89567d
김수현 <kimsuhyeon@gimsuhyeon-ui-MacBookPro.local>
2025-08-26 00:44:34 +0900

    feat : GREEN --> 매주 반복 컴포넌트 작성

--- src/utils/repeatingDates.ts
+++ src/utils/repeatingDates.ts
@@ -7,11 +7,19 @@ export function repeatingDates(
   if (repeat.type === 'daily') {
     const dates = [];
     const start = new Date(startDate);
-    console.log(start);
     const end = new Date(repeat.endDate!);
     for (let d = start; d <= end; d.setDate(d.getDate() + repeat.interval)) {
       dates.push(d.toISOString().split('T')[0]);
-      console.log(dates);
+    }
+    return dates;
+  }
+
+  if (repeat.type === 'weekly') {
+    const dates = [];
+    const start = new Date(startDate);
+    const end = new Date(repeat.endDate!);
+    for (let d = start; d <= end; d.setDate(d.getDate() + repeat.interval * 7)) {
+      dates.push(d.toISOString().split('T')[0]);
     }
     return dates;
   }
